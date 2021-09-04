d5b62f485b0eb4d352d18dc19ada47aa46b271c5
ericwafula <eric.wafula@moringaschool.com>
2021-09-04 14:15:59 +0300

    Added onclick method and initial MainActivity intent

--- app/src/main/java/com/moringaschool/forexexchange/MainActivity.java
+++ app/src/main/java/com/moringaschool/forexexchange/MainActivity.java
@@ -2,13 +2,35 @@ package com.moringaschool.forexexchange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button mLogin;
+    EditText mUsername;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLogin = (Button) findViewById(R.id.loginButton);
+        mUsername = (EditText) findViewById(R.id.username);
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUsername.getText().toString();
+                Log.d(TAG, username);
+                Toast.makeText(MainActivity.this, username, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, converter.class);
+                startActivity(intent);
+            }
+        });
     }
 }
