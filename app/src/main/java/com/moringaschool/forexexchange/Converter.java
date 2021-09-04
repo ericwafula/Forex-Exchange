1393ab1827efe2a17531fe2ac96906d4505dd776
ericwafula <eric.wafula@moringaschool.com>
2021-09-05 02:35:14 +0300

    Added ListView variable to converter class

--- app/src/main/java/com/moringaschool/forexexchange/Converter.java
+++ app/src/main/java/com/moringaschool/forexexchange/Converter.java
@@ -5,33 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class Converter extends AppCompatActivity {
-    TextView user;
+    TextView mUser;
     String[] currencies = new String[] {
             "US Dollar(USD)", "Canadian DollarCAD", "British Pound(GBP)", "Euro(EUR)",
             "Australian Dollar(AUD)", "Swiss Franc(CHF)", "New Zealand Dollar(NZD)",
             "Japanese Yen(JPY)"
     };
 
-
+    ListView mCurrencyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
 
-        user = (TextView) findViewById(R.id.firstName);
+        mUser = (TextView) findViewById(R.id.firstName);
+        mCurrencyList = (ListView) findViewById(R.id.currencies);
+
         Intent intent = getIntent();
         String firstName = intent.getStringExtra("firstName");
         String userName = intent.getStringExtra("username");
 
         if (firstName.length() != 0){
-            user.setText("Welcome " + firstName);
+            mUser.setText("Welcome " + firstName);
         } else {
-            user.setText("Welcome " + userName);
+            mUser.setText("Welcome " + userName);
         }
+
 
 
     }
