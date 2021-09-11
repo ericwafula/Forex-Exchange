69443bd5b092d307ce4b24ae348839108e6b8955
ericwafula <ericwathome007@gmail.com>
2021-09-11 16:07:45 +0300

    Added Base Url and Api Key to Constants file

--- app/src/main/java/com/moringaschool/forexexchange/network/Constants.java
+++ app/src/main/java/com/moringaschool/forexexchange/network/Constants.java
@@ -1,4 +1,6 @@
 package com.moringaschool.forexexchange.network;
 
 public class Constants {
+    public static final String EXCHANGE_RATE_BASE_URL = "https://v6.exchangerate-api.com/v6/";
+    public static final String EXCHANGE_RATE_API_KEY = "917f5d3adda6f614d590e1c1";
 }
