0421096b9079c9958f58c26342fff26f9d64530f
ericwafula <ericwathome007@gmail.com>
2021-09-19 16:10:26 +0300

    Added authState listener and account creation

--- app/src/main/java/com/moringaschool/forexexchange/ui/CreateAccount.java
+++ app/src/main/java/com/moringaschool/forexexchange/ui/CreateAccount.java
@@ -1,24 +1,31 @@
 package com.moringaschool.forexexchange.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.*;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.forexexchange.R;
 
 public class CreateAccount extends AppCompatActivity implements View.OnClickListener{
-    @BindView(com.moringaschool.forexexchange.R.id.firstName) EditText mFirstName;
-    @BindView(com.moringaschool.forexexchange.R.id.lastName) EditText mLastName;
-    @BindView(com.moringaschool.forexexchange.R.id.userName) EditText mUsername;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @BindView(com.moringaschool.forexexchange.R.id.name) EditText mName;
     @BindView(com.moringaschool.forexexchange.R.id.email) EditText mEmail;
     @BindView(com.moringaschool.forexexchange.R.id.password) EditText mPassword;
+    @BindView(R.id.confirmPassword) EditText mConfirmPassword;
     @BindView(com.moringaschool.forexexchange.R.id.signupButton) Button signupButton;
     @BindView(com.moringaschool.forexexchange.R.id.alreadyHaveAnAccount) TextView alreadyHaveAnAccount;
 
@@ -33,28 +40,65 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
 
         signupButton.setOnClickListener(this);
         alreadyHaveAnAccount.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        createAuthStateListener();
     }
 
     @Override
     public void onClick(View view) {
         if (view == signupButton){
-            String firstName = mFirstName.getText().toString();
-            String lastName = mLastName.getText().toString();
-            String username = mUsername.getText().toString();
-            String email = mEmail.getText().toString();
-            String password = mPassword.getText().toString();
-
-            if(!(firstName.isEmpty() || lastName.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty())){
-                Intent intent = new Intent(CreateAccount.this, Converter.class);
-                intent.putExtra("firstName", firstName);
-                startActivity(intent);
-            } else {
-                Toast.makeText(CreateAccount.this, "All fields must be filled", Toast.LENGTH_LONG).show();
-            }
-
+            createNewUser();
         } else if (view == alreadyHaveAnAccount){
             Intent intent = new Intent(CreateAccount.this, MainActivity.class);
             startActivity(intent);
+        }
+    }
+
+    private void createNewUser(){
+        String name = mName.getText().toString();
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        String confirmPassword = mConfirmPassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()){
+                        Log.d(TAG, "Authentication successful");
+                    } else {
+                        Toast.makeText(CreateAccount.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void createAuthStateListener(){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null){
+                    Intent intent = new Intent(CreateAccount.this, Converter.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+//
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 }
