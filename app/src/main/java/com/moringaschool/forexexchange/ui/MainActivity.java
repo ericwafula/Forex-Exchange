027de27380725f10d60cb7c687a1e95e7f89864c
ericwafula <ericwathome007@gmail.com>
2021-09-19 21:50:46 +0300

    Added login authentication

--- app/src/main/java/com/moringaschool/forexexchange/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/forexexchange/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moringaschool.forexexchange.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -8,26 +9,35 @@ import butterknife.ButterKnife;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moringaschool.forexexchange.R;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(com.moringaschool.forexexchange.R.id.loginButton) Button mLogin;
-    @BindView(com.moringaschool.forexexchange.R.id.username) EditText mUsername;
+    @BindView(R.id.email) EditText mEmail;
     @BindView(com.moringaschool.forexexchange.R.id.password) EditText mPassword;
     @BindView(com.moringaschool.forexexchange.R.id.createAnAccountText) TextView createAnAccount;
-    @BindView(R.id.forexImage) ImageView mForexImage;
+    @BindView(R.id.firebaseProgressBar) ProgressBar mSignInProgressBar;
+    @BindView(R.id.loadingTextView) TextView mLoadingSignUp;
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,32 +47,89 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         ButterKnife.bind(this);
 
-        Picasso.get().load(R.drawable.exchange).into(mForexImage);
+//        Picasso.get().load(R.drawable.exchange).into(mForexImage);
 
         mLogin.setOnClickListener(this);
         createAnAccount.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null){
+                    Intent intent = new Intent(MainActivity.this, Converter.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 
     @Override
     public void onClick(View view) {
         if (view == mLogin){
-            String username = mUsername.getText().toString();
-            String password = mPassword.getText().toString();
-
-
-
-            if(!(username.isEmpty() || password.isEmpty())){
-                Intent intent = new Intent(MainActivity.this, Converter.class);
-                intent.putExtra("username", username);
-                startActivity(intent);
-            } else{
-                Toast.makeText(MainActivity.this, "username and password fields are not supposed to be empty", Toast.LENGTH_LONG).show();
-            }
-
-
+            loginWithPassword();
+            showProgressBar();
         } else if (view == createAnAccount){
             Intent intent = new Intent(MainActivity.this, CreateAccount.class);
             startActivity(intent);
+        }
+    }
+
+    private void loginWithPassword(){
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            mEmail.setError("Please enter your email");
+            return;
+        }
+
+        if (password.isEmpty()){
+            mPassword.setError("Password cannot be blank");
+        }
+
+        showProgressBar();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        hideProgressBar();
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if(!task.isSuccessful()){
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void showProgressBar() {
+        mSignInProgressBar.setVisibility(View.VISIBLE);
+        mLoadingSignUp.setVisibility(View.VISIBLE);
+
+        mLoadingSignUp.setText("Log in you in");
+    }
+
+    private void hideProgressBar() {
+        mSignInProgressBar.setVisibility(View.GONE);
+        mLoadingSignUp.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 }
