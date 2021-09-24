72869b86a44b44b0b978d0a71fff52447daa6c3c
ericwafula <ericwathome007@gmail.com>
2021-09-24 16:32:12 +0300

    Added default ItemTouchHelper Callback methods

--- app/src/main/java/com/moringaschool/forexexchange/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/moringaschool/forexexchange/util/SimpleItemTouchHelperCallback.java
@@ -1,5 +1,23 @@
 package com.moringaschool.forexexchange.util;
 
-public class SimpleItemTouchHelperCallback {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback{
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        return 0;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
 }
